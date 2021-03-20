14a4f5fcd733033b95324f4b492b2753c5fd5e0e
berghdavid <berghdavid@hotmail.com>
2021-03-20 23:21:38 +0100

    Implemented Rook, bishop + knight

--- lib/Piece.rb
+++ lib/Piece.rb
@@ -11,10 +11,6 @@ class Piece
 
     end
 
-    def piece_pinned?(board)
-
-    end
-
     def opp_color()
         if(@color == "White")
             return "Black"
@@ -25,6 +21,38 @@ class Piece
 
     def within_bounds?(to)
         return (to[0] >= 1 && to[0] <= 8 && to[1] >= 1 && to[1] <= 8)
+    end
+
+    def in_diagonal?(to)
+        diagonal_squares = []
+
+        x1 = @x
+        y1 = @y
+        while(x1 != 1 && y1 != 1) #Replace with if x > y else
+            x1 -= 1
+            y1 -= 1
+        end
+        while(x1 <= 8 && y1 <= 8)
+            diagonal_squares += [[x1, y1]]
+            x1 += 1
+            y1 += 1
+        end
+
+        x2 = @x
+        y2 = @y
+        while(x2 != 1 && y2 != 8)
+            x2 -= 1
+            y2 += 1
+        end
+        while(x2 <= 8 && y2 >= 1)
+            diagonal_squares += [[x2, y2]]
+            x2 += 1
+            y2 -= 1
+        end
+        diagonal_squares = diagonal_squares.select {
+            |sq| sq != [@x, @y] # Removes square [@x, @y]
+        }
+        return diagonal_squares.include?(@x, @y)
     end
 
     def get_initials()
@@ -88,11 +116,11 @@ end
 
 class Rook < Piece
     def possible_move?(board, to)
-        if(!within_bounds?(to))
+        if(!within_bounds?(to) || [@x, @y] != to)
             return false
         end
 
-        if(to[0] == @x || to[1] == @y && [@x, @y] != to)
+        if(to[0] == @x || to[1] == @y)
             return empty_path?(board, to)
         end
     end
@@ -123,8 +151,49 @@ class Rook < Piece
                 end
                 i += dir
             end
-            if(!board[[i, @y]].nil? && board[[i, @y]].color != opp_color())
+            if(!board[[i, @y]].nil? && board[[i, @y]].color == @color)
                 return false
+            end
+        end
+        return false
+    end
+
+    def all_possible_moves(board)
+        all_moves = []
+        (1..8).each do |xs|
+            to = [xs, @y]
+            if(possible_move?(board, to))
+                all_moves += [to]
+            end
+        end
+        (1..8).each do |ys|
+            to = [@x, ys]
+            if(possible_move?(board, to))
+                all_moves += [to]
+            end
+        end
+        return all_moves
+    end
+
+    def get_initials()
+        return "R"
+    end
+end
+
+class Knight < Piece
+    def possible_move?(board, to)
+        if(!within_bounds?(to) || [@x, @y] == to)
+            return false
+        end
+
+        diff_x = (to[0] - @x).abs
+        diff_y = (to[1] - @y).abs
+
+        if((diff_x == 1 && diff_y == 2) || (diff_x == 2 && diff_y == 1))
+            if(!board[[i, @y]].nil? && board[[i, @y]].color == @color)
+                return false
+            else
+                return true
             end
         end
         return false
@@ -135,23 +204,75 @@ class Rook < Piece
     end
 
     def get_initials()
-        return "R"
-    end
-end
-
-class Knight < Piece
-    def reachable_square?(square)
-        return (@posX == square[0] || @posY == square[1])
-    end
-
-    def get_initials()
         return "N"
     end
 end
 
 class Bishop < Piece
-    def reachable_square?(square)
-        return (@posX == square[0] || @posY == square[1])
+    def possible_move?(board, to)
+        if(!within_bounds?(to))
+            return false
+        end    
+
+        if(in_diagonal?(to) && [@x, @y] != to)
+            return empty_path?(board, to)
+        end
+    end
+
+    def empty_path?(board, to)
+        if(to[0] > @x)
+            x_dir = 1
+        else
+            x_dir = -1
+        end
+        if(to[1] > @y)
+            y_dir = 1
+        else
+            y_dir = -1
+        end
+        temp = [@x, @y]
+        while(temp != to)
+            if(!board[temp].nil?)
+                return false
+            end
+            temp = [temp[0] + x_dir, temp[1] + y_dir]
+        end
+        if(!board[to].nil? && board[to].color == @color)
+            return false
+        end
+        return true
+    end
+
+    def all_possible_moves()
+        diagonal_squares = []
+
+        x1 = @x
+        y1 = @y
+        while(x1 != 1 && y1 != 1) #Replace with if x > y else
+            x1 -= 1
+            y1 -= 1
+        end
+        while(x1 <= 8 && y1 <= 8)
+            diagonal_squares += [[x1, y1]]
+            x1 += 1
+            y1 += 1
+        end
+
+        x2 = @x
+        y2 = @y
+        while(x2 != 1 && y2 != 8)
+            x2 -= 1
+            y2 += 1
+        end
+        while(x2 <= 8 && y2 >= 1)
+            diagonal_squares += [[x2, y2]]
+            x2 += 1
+            y2 -= 1
+        end
+        possible_moves = diagonal_squares.select {
+            |sq| sq != [@x, @y] && possible_move?(board, sq)
+        }
+        return possible_moves
     end
 
     def get_initials()
